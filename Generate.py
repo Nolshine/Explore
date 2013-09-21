e22608b3022c2a9217197dfcee30bda7ee568f48
Ariel M Shaked <mtr131@gmail.com>
2013-09-21 14:35:07 +0100

    made a messy world generator, ready for 3 lands

--- Generate.py
+++ Generate.py
@@ -3,3 +3,17 @@
 Will hopefully generate a nice 1000*1000 block world to walk on,
 then pickle it and store it in a file.
 """
+from random import choice
+import pickle
+
+types = [0,1,2]
+world = []
+
+for i in range(1000):
+    world.append([])
+    for n in range(1000):
+        world[i].append(choice(types))
+
+name = raw_input("Input filename then press enter, please.\n")
+with open(name + ".wld", 'w') as f:
+    pickle.dump(world, f)
