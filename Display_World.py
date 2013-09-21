bec7a17c89d70e79a660da5ad83e519255bdffc9
Ariel M Shaked <mtr131@gmail.com>
2013-09-21 13:55:16 +0100

    initial commit

---
+++ Display_World.py
@@ -0,0 +1,5 @@
+""" Display_World.py
+
+    Takes a generated world and makes a lovely picture of it using pixels.
+    Hopefully.
+    """
---
+++ Generate.py
@@ -0,0 +1,5 @@
+""" Generate.py
+
+Will hopefully generate a nice 1000*1000 block world to walk on,
+then pickle it and store it in a file.
+"""
